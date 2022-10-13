7a7cec37ee171b8262b60cb40d8fb081341e85f3
seesaw <ltg1710@gmail.com>
2022-10-13 17:15:04 +0800

    01 builder pass 👌
    仅仅是欺骗展开器，new了一个`TokenStream`而已。

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -2,7 +2,6 @@ use proc_macro::TokenStream;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
-
-    unimplemented!()
+    let _st = syn::parse_macro_input!(input as syn::DeriveInput);
+    TokenStream::new()
 }
