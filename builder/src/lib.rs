b13429c28707d86970b1c7602371a6b8580fe3a7
seesaw <ltg1710@gmail.com>
2022-10-16 14:21:50 +0800

    05 builder 06 test pass🎱

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -2,6 +2,26 @@ use proc_macro::TokenStream;
 use quote::{format_ident, quote};
 use syn::{Fields};
 
+// Option<String> => Some(String)
+fn inner_type_option<'a>(ty:&'a syn::Type) -> Option<&'a syn::Type> {
+    if let syn::Type::Path(syn::TypePath { qself: None, ref path,}) = ty {
+        if path.segments.len() != 1 || path.segments[0].ident != *"Option" {
+            return None;
+        }
+
+        if let syn::PathArguments::AngleBracketed(ref inner_type) = path.segments[0].arguments {
+            if inner_type.args.len() != 1 {
+                return None;
+            }
+
+            if let syn::GenericArgument::Type(ref ty) = inner_type.args[0] {
+                return Some(ty);
+            }
+        }
+    }
+    None
+}
+
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let ast = syn::parse_macro_input!(input as syn::DeriveInput);
@@ -21,8 +41,12 @@ pub fn derive(input: TokenStream) -> TokenStream {
 
     let builder_fields_declare = fields.iter().map(|f| {
         let ident = &f.ident;
-        let ty = &f.ty;
-        quote!(#ident: std::option::Option<#ty>)
+        let ty:&syn::Type = &f.ty;
+        if inner_type_option(ty).is_some() {
+            quote!(#ident: #ty)
+        } else {
+            quote!(#ident: std::option::Option<#ty>)
+        }
     });
 
     let builder_fields_default = fields.iter().map(|f| {
@@ -33,19 +57,36 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let builder_fields_setters = fields.iter().map(|f| {
         let ident = &f.ident;
         let ty = &&f.ty;
-        quote!(
-            pub fn #ident(&mut self, #ident: #ty) -> &mut Self {
-                self.#ident = Some(#ident);
-                self
-            }
-        )
+        let inner_ty = inner_type_option(ty);
+        if let Some(inner_ty) = inner_ty {
+            quote!(
+                pub fn #ident(&mut self, #ident: #inner_ty) -> &mut Self {
+                    self.#ident = Some(#ident);
+                    self
+                }
+            )
+        } else {
+            quote!(
+                pub fn #ident(&mut self, #ident: #ty) -> &mut Self {
+                    self.#ident = Some(#ident);
+                    self
+                }
+            )
+        }
     });
 
     let builder_fields_clauses = fields.iter().map(|f| {
         let ident = &f.ident;
-        quote!(
-            #ident: self.#ident.clone().ok_or(format!("{} field is missing", stringify!(#ident)))?
-        )
+        let ty = &&f.ty;
+        if inner_type_option(ty).is_some() {
+            quote!(
+                #ident: self.#ident.clone()
+            )
+        } else {
+            quote!(
+                #ident: self.#ident.clone().ok_or(format!("{} field is missing", stringify!(#ident)))?
+            )
+        }
     });
 
     let output = quote!(
