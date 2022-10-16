5b896e8b13dd74919469f3e2606047eb473b17f8
seesaw <ltg1710@gmail.com>
2022-10-16 17:30:12 +0800

    06 builder 07 test pass💪

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -2,10 +2,46 @@ use proc_macro::TokenStream;
 use quote::{format_ident, quote};
 use syn::{Fields};
 
+fn get_name_value(name:&str, attr:&syn::Attribute) -> Result<syn::Ident, syn::__private::TokenStream2>{
+    match attr.parse_meta() {
+        Ok(syn::Meta::List(list)) => {
+            if list.nested.len() != 1 {
+                return Err(syn::Error::new_spanned(list.nested, "expected `builder(each = \"...\")`").to_compile_error());
+            }
+            Ok(match &list.nested[0] {
+                syn::NestedMeta::Meta(syn::Meta::NameValue(nv)) => {
+                    if !nv.path.is_ident(name) {
+                        return Err(syn::Error::new_spanned(list.nested, "expected `builder(each = \"...\")`").to_compile_error());
+                    } else {
+                        match &nv.lit {
+                            syn::Lit::Str(val) => {
+                                format_ident!("{}", val.value(), span = val.span())
+                            },
+                            _ => {
+                                return Err(syn::Error::new_spanned(list.nested, "expected `builder(each = \"...\")`").to_compile_error());
+                            }
+                        }
+                    }
+                }
+                _ => {
+                    return Err(syn::Error::new_spanned(list.nested, "expected `builder(each = \"...\")`").to_compile_error());
+                }
+            })
+        }
+        _ => Err(syn::Error::new_spanned(&attr.tokens,"expected `builder(each = \"...\")`").to_compile_error()),
+    }
+}
+
+// #[builder(each="what")]
+fn get_attr_builder(field:& syn::Field) ->Option<&syn::Attribute> {
+    let attrs = &field.attrs;
+    attrs.iter().find(|&attr| attr.path.segments.len() == 1 && attr.path.segments[0].ident == "builder")
+}
+
 // Option<String> => Some(String)
-fn inner_type_option<'a>(ty:&'a syn::Type) -> Option<&'a syn::Type> {
+fn inner_type<'a>(outer:&'a str, ty:&'a syn::Type) -> Option<&'a syn::Type> {
     if let syn::Type::Path(syn::TypePath { qself: None, ref path,}) = ty {
-        if path.segments.len() != 1 || path.segments[0].ident != *"Option" {
+        if path.segments.len() != 1 || path.segments[0].ident != *outer {
             return None;
         }
 
@@ -22,7 +58,7 @@ fn inner_type_option<'a>(ty:&'a syn::Type) -> Option<&'a syn::Type> {
     None
 }
 
-#[proc_macro_derive(Builder)]
+#[proc_macro_derive(Builder, attributes(builder))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let ast = syn::parse_macro_input!(input as syn::DeriveInput);
     let derived_obj_ident = ast.ident;
@@ -42,7 +78,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let builder_fields_declare = fields.iter().map(|f| {
         let ident = &f.ident;
         let ty:&syn::Type = &f.ty;
-        if inner_type_option(ty).is_some() {
+        if inner_type("Option", ty).is_some() || get_attr_builder(f).is_some() {
             quote!(#ident: #ty)
         } else {
             quote!(#ident: std::option::Option<#ty>)
@@ -51,34 +87,63 @@ pub fn derive(input: TokenStream) -> TokenStream {
 
     let builder_fields_default = fields.iter().map(|f| {
         let ident = &f.ident;
-        quote!(#ident: std::option::Option::None)
+                
+        if get_attr_builder(f).is_some() {
+            quote!(#ident: std::vec::Vec::new())
+        } else {
+            quote!(#ident: std::option::Option::None)
+        }
     });
 
-    let builder_fields_setters = fields.iter().map(|f| {
+    let builder_fields_setters = fields.iter().filter_map(|f| {
         let ident = &f.ident;
         let ty = &&f.ty;
-        let inner_ty = inner_type_option(ty);
+        let inner_ty = inner_type("Option", ty);
+        if get_attr_builder(f).is_some() {
+            return None;
+        }
         if let Some(inner_ty) = inner_ty {
-            quote!(
+            Some(quote!(
                 pub fn #ident(&mut self, #ident: #inner_ty) -> &mut Self {
                     self.#ident = Some(#ident);
                     self
                 }
-            )
+            ))
         } else {
-            quote!(
+            Some(quote!(
                 pub fn #ident(&mut self, #ident: #ty) -> &mut Self {
                     self.#ident = Some(#ident);
                     self
                 }
-            )
+            ))
+        }
+    });
+
+    let builder_vecs_setters = fields.iter().filter_map(|f| {
+        let ident = &f.ident;
+        let ty = &f.ty;
+        if let Some(attr) = get_attr_builder(f) {
+            let indent = get_name_value("each", attr);
+            if let Ok(each) = indent {
+                let inner_ty = inner_type("Vec", ty).unwrap();
+                Some(quote!(
+                    pub fn #each(&mut self, #each: #inner_ty) -> &mut Self {
+                        self.#ident.push(#each);
+                        self
+                    }
+                ))
+            }else {
+                Some(indent.err().unwrap())
+            }
+        } else {
+            None
         }
     });
 
     let builder_fields_clauses = fields.iter().map(|f| {
         let ident = &f.ident;
         let ty = &&f.ty;
-        if inner_type_option(ty).is_some() {
+        if inner_type("Option", ty).is_some() || get_attr_builder(f).is_some() {
             quote!(
                 #ident: self.#ident.clone()
             )
@@ -97,6 +162,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
 
         impl #derived_obj_builder_ident {
             #(#builder_fields_setters)*
+            #(#builder_vecs_setters)*
             pub fn build(&mut self) -> std::result::Result<#derived_obj_ident, ::std::boxed::Box<dyn ::std::error::Error>> {
                 std::result::Result::Ok(
                     #derived_obj_ident {
